e8b8e94c42f39db5d4e6a74e33ddbedda80417f7
CuongN + KyP <ky.phan@eastagile.com>
2022-10-28 17:17:48 +0700

    Beautify `commitment.cjs`

---
+++ sample/commitment.cjs
@@ -0,0 +1,23 @@
+const {utils} = require('ethers');
+
+DOMAIN_NAME = process.argv[2];
+OWNER_ADDR = process.argv[3];
+SECRET = process.argv[4];
+
+const fromHexString = (hexString) =>
+  Array.from(
+      hexString
+          .match(/.{1,2}/g)
+          .filter((byte) => byte !== '0x')
+          .map((byte) => parseInt(byte, 16)),
+  );
+const encoder = new TextEncoder();
+const concatArray = new Uint8Array([
+  ...encoder.encode(DOMAIN_NAME),
+  ...fromHexString(OWNER_ADDR),
+  ...encoder.encode(SECRET),
+]);
+
+console.log(
+    '[' + fromHexString(utils.keccak256(concatArray).slice(2)).join() + ']',
+);
