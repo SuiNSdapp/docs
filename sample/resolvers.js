6e913c5e26b397130e831a16d9d4b9fdde236401
TrungH+AnhT <trung.hoang+anh.q.tran@eastagile.com>
2023-01-10 12:37:22 +0700

    Update resolvers.js

--- sample/resolvers.js
+++ sample/resolvers.js
@@ -1,12 +1,13 @@
 import get from 'lodash/get';
+import { JsonRpcProvider } from '@mysten/sui.js'
 
-const PACKAGE_ADDRESS = process.env.REACT_APP_PACKAGE_ADDRESS;
-const REGISTRY_ADDRESS = process.env.REACT_APP_REGISTRY_ADDRESS;
+const PACKAGE_ADDRESS = '0xf44f6b29c763a496bb29d560b637aee69f90e8ed'; // Replace this with the current package address
+const REGISTRY_ADDRESS = '0x866a120cf14fc500a448b9461f53592477ae4019'; // Replace this with the current registry object address
 const SENDER = '0x01'; // Replace this with a valid SUI address
-const DEV_INSPECT_RESULT_PATH = 'results.Ok[0][1].returnValues[0][0]';
+const DEV_INSPECT_RESULT_PATH_0 = 'results.Ok[0][1].returnValues[0][0]';
 const DEV_INSPECT_RESULT_PATH_1 = 'results.Ok[0][1].returnValues[1][0]';
 
-const suiProvider = new JsonRpcProvider(process.env.REACT_APP_FULL_NODE_URL);
+const suiProvider = new JsonRpcProvider('https://fullnode.devnet.sui.io/');
 
 const toHexString = (byteArray) =>
   byteArray?.length > 0 ? Array.from(byteArray, (byte) => ('0' + (byte & 0xff).toString(16)).slice(-2)).join('') : '';
@@ -16,12 +17,7 @@ const toString = (byteArray) =>
 
 const trimAddress = (address) => String(address?.match(/0x0{0,}([\w\d]+)/)?.[1]);
 
-const toFullAddress = (trimmedAddress) =>
-  trimmedAddress
-    ? `0x${Array(40 - trimmedAddress.length)
-        .fill(0)
-        .join('')}${trimmedAddress}`
-    : '';
+const toFullAddress = (trimmedAddress) => (trimmedAddress ? `0x${trimmedAddress.padStart(40, '0')}` : '');
 
 export const getName = async (address, sender = SENDER) => {
   const resolverBytes = get(
@@ -46,7 +42,7 @@ export const getName = async (address, sender = SENDER) => {
         typeArguments: [],
         arguments: [resolver, address],
       }),
-      DEV_INSPECT_RESULT_PATH,
+      DEV_INSPECT_RESULT_PATH_0,
     ),
   );
   return name;
@@ -74,7 +70,7 @@ export const getAddress = async (domain, sender = SENDER) => {
       typeArguments: [],
       arguments: [resolver, domain],
     }),
-    DEV_INSPECT_RESULT_PATH,
+    DEV_INSPECT_RESULT_PATH_0,
   );
   return addr?.every((e) => !e) ? '' : toFullAddress(toHexString(addr));
 };
